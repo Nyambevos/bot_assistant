61b221037a9e2e5ee3a91c9af16b9678d2e3f500
Nyambevos <ruslan.bilokoniuk@gmail.com>
2023-12-27 11:52:50 +0200

    Added functionality to save to file and search by keyword

---
+++ address_book.py
@@ -0,0 +1,143 @@
+from collections import UserDict
+from datetime import datetime
+import pickle
+
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return str(self.__value)
+
+    @property
+    def value(self):
+        return self.__value
+
+    @value.setter
+    def value(self, value):
+        if self.is_valid(value):
+            self.__value = value
+        else:
+            self.__value = None
+
+    def is_valid(self, value):
+        return True
+
+
+class Name(Field):
+    def is_valid(self, value):
+        if value.isalnum():
+            return True
+        raise ValueError("Incorrect name")
+
+
+class Phone(Field):
+    def is_valid(self, value):
+        if len(value) != 10 or not value.isdigit():
+            raise ValueError("Incorrect phone number")
+        return True
+
+
+class Birthday(Field):
+    def is_valid(self, value):
+        if value is not None:
+            birthday = datetime.strptime(value, "%d-%m-%Y")
+            if birthday > datetime.now():
+                raise ValueError("Incorrect birthday's date")
+            return True
+
+
+class Record:
+    def __init__(self, name, birthday=None):
+        self.name = Name(name)
+        self.birthday = Birthday(birthday)
+        self.phones = []
+
+    def __str__(self):
+        name = self.name.value
+        phones = '; '.join(p.value for p in self.phones)
+        birthday = self.birthday.value
+        return f"Contact name: {name}, phones: {phones}, birthday: {birthday}"
+
+    def add_phone(self, phone):
+        self.phones.append(Phone(phone))
+
+    def remove_phone(self, phone):
+        self.phones.remove(self.find_phone(phone))
+
+    def edit_phone(self, old_phone, new_phone):
+        obj_old_phone = self.find_phone(old_phone)
+
+        if obj_old_phone is None:
+            raise ValueError
+
+        index_old_phone = self.phones.index(obj_old_phone)
+
+        self.phones[index_old_phone] = Phone(new_phone)
+
+    def find_phone(self, phone):
+        ph = list(filter(lambda ph: ph.value == phone, self.phones))
+
+        if len(ph):
+            return ph[0]
+
+        return None
+
+    def days_to_birthday(self):
+        if self.birthday is not None:
+            birthday = datetime.strptime(
+                self.birthday.value, "%d-%m-%Y")
+
+            next_birthday = birthday.replace(
+                year=datetime.now().year)
+
+            if next_birthday < datetime.now():
+                next_birthday = next_birthday.replace(
+                    year=next_birthday.year + 1)
+
+            difference = next_birthday.date() - datetime.now().date()
+
+            return difference.days
+
+        return None
+
+
+class AddressBook(UserDict):
+    def add_record(self, record):
+        self.data.update({record.name.value: record})
+
+    def find(self, name):
+        return self.data.get(name)
+
+    def delete(self, name):
+        self.data.pop(name, None)
+
+    def iterator(self, amount=1):
+        index = 0
+        records_list = list(self.data.values())
+
+        while index < len(records_list):
+            yield records_list[index:index + amount]
+            index += amount
+
+    def save_to_file(self, filename):
+        with open(filename, "wb") as file:
+            pickle.dump(self.data, file)
+
+    def load_from_file(self, filename):
+        with open(filename, "rb") as file:
+            self.data = pickle.load(file)
+
+    def find_by_key(self, key):
+        match_records = []
+        for record in self.data.values():
+            if key in record.name.value:
+                match_records.append(record)
+                continue
+            else:
+                for phone in record.phones:
+                    if key in phone.value:
+                        match_records.append(record)
+                        break
+        return match_records if match_records else None
--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-contacts = {}
+import sys
+from pathlib import Path
 
+from address_book import AddressBook, Record
+
+contacts = AddressBook()
+
+FILENAME = "contacts.bin"
 
 def input_error(func):
     def wrapper(*args, **kwargs):
@@ -8,22 +14,18 @@ def input_error(func):
 
         try:
             return func(*args, **kwargs)
-        except TypeError as str_error:
-            if "positional argument" in str_error.args[0]:
-                if "2" in str_error.args[0]:
-                    return "Give me name and phone please"
-                elif "1" in str_error.args[0]:
-                    return "Enter user name"
-                else:
-                    return str_error.args[0]
+        except:
+            return ("Oops. Something went wrong.")
 
-        except IndexError as str_error:
-            return str_error.args[0]
     return wrapper
 
 
 def hello():
     return "How can I help you?"
+
+def exit():
+    contacts.save_to_file(FILENAME)
+    sys.exit()
 
 
 @input_error
@@ -31,31 +33,43 @@ def add_new_contact(name, phone):
     if name in contacts:
         raise IndexError(f"I already have a contact named: {name}")
 
-    contacts[name] = phone
+    contacts.add_record(Record(name))
+    contacts[name].add_phone(phone)
+
     return f"I have added a new contact with the name \"{name.capitalize()}\""
 
 
 @input_error
-def change_phone(name, phone):
-    if name not in contacts:
-        raise IndexError(f"Oops. I couldn't find {name.capitalize()}.")
+def change_phone(name, old_phone, new_phone):
+    contacts[name].edit_phone(old_phone, new_phone)
 
-    contacts[name] = phone
-    return f"The phone number for contact: {name} has been changed to: {phone}"
+    return f"The phone number for contact: {name} has been changed to: {new_phone}"
 
 
 @input_error
 def show_phone(name):
-    if name not in contacts:
-        raise IndexError(f"Oops. I couldn't find {name.capitalize()}.")
+    phones = []
+    for phone in contacts[name].phones:
+        phones.append(phone.value)
 
-    return f"Name: {name.capitalize()} Phone: {contacts[name]}"
+    return f"Name: {name.capitalize()} Phone: {' '.join(phones)}"
 
 
 @input_error
 def show_all():
-    return "\n".join(f"{name} {phone}" for name, phone in contacts.items())
+    iter = contacts.iterator()
+    
+    for contact in iter:
+        print(contact[0])
 
+    return "Done"
+
+@input_error
+def find(keyword):
+    found_contacts = contacts.find_by_key(keyword)
+    for contact in found_contacts:
+        print(contact)
+    return "Done"
 
 def non_exist_command(*_):
     return "Oops. I don't know this command yet."
@@ -65,15 +79,18 @@ def main():
     commands = {
 
         "add": add_new_contact,
-        "hello": exit,
+        "hello": hello,
         "change": change_phone,
         "phone": show_phone,
         "show all": show_all,
+        "find": find,
 
         "exit": exit,
         "good bye": exit,
         "close": exit
     }
+    if Path(FILENAME).is_file():
+        contacts.load_from_file(FILENAME)
 
     while True:
         user_string = input(">>> ").lower()
